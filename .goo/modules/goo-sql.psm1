57f838281cd19d163ef9dba72eabcc9e68853c8b
Andre Sharpe <andre.sharpe@gmail.com>
2022-11-01 13:58:10 +0100

    Added checks for Sql module installation

--- .goo/modules/goo-sql.psm1
+++ .goo/modules/goo-sql.psm1
@@ -15,15 +15,24 @@ class GooSql {
         return "Goo"+$this.Name+"Module"
     }
 
-    [void] Query( [string]$connString, [string]$sql ){
+    [bool] EnsureModuleIsInstalled() {
+
         if( -not ((Get-Module -ListAvailable -Name "SqlServer"))) {
             $this.Goo.Error("Module not found. Please run 'Install-Module -Name SqlServer' as admin")
+            return $false;
         }
+        return $true;
+    }
+
+
+    [void] Query( [string]$connString, [string]$sql ){
+        $this.EnsureModuleIsInstalled()
         Invoke-Sqlcmd -ConnectionString $connString -Query $sql | Format-Table -AutoSize | Out-Host
         if( -not $? ) { $this.Goo.Error( "Sql error." ) }
     }
 
     [bool] TestConnection([string]$connString) {
+        $this.EnsureModuleIsInstalled()
         try {
             $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $connString
             $sqlConnection.Open()
@@ -37,6 +46,7 @@ class GooSql {
     }
 
     [bool] WaitForConnection([string]$connString, [int]$seconds) {
+        $this.EnsureModuleIsInstalled()
         $ret = $false
         for($i = $seconds; $i -gt 0; $i--) {
             if($this.TestConnection($connString)) {
